3a09026cb481b153fc6f96b992b534ed0ae76b24
Farid Ahmed <faridahmed.tech@gmail.com>
2023-01-06 22:19:10 +0530

    bug fix

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -30,11 +30,11 @@ export default function Form(props){
             var countSpaces = 0;
         for(var i=0; i<fullName.length; i++){
             if(fullName.charCodeAt(i) === 32)countSpaces = 1;
-            if(fullName.charCodeAt(i) === 32 || fullName.charCodeAt(i) >= 65 && fullName.charCodeAt(i) <= 90 || fullName.charCodeAt(i) >= 97 && fullName.charCodeAt(i) <= 122){}
+            if((fullName.charCodeAt(i) === 32 || fullName.charCodeAt(i) >= 65) && (fullName.charCodeAt(i) <= 90 || fullName.charCodeAt(i) >= 97) && fullName.charCodeAt(i) <= 122){}
             else {
                 setFullNameErr(true);
             }
-            if(fullName.charCodeAt(i+1)!=32 && i < fullName.length-1 && fullName.charCodeAt(i+1) < 65 || fullName.charCodeAt(i+1) > 90 && fullName.charCodeAt(i) === 32){
+            if(fullName.charCodeAt(i+1)!==32 && i < fullName.length-1 && (fullName.charCodeAt(i+1) < 65 || fullName.charCodeAt(i+1) > 90) && fullName.charCodeAt(i) === 32){
                 setFullNameErr(true);
             }
         }
@@ -44,12 +44,12 @@ export default function Form(props){
         //Card Number Format Error
         if(cardNo){
             var countNums = 0;
-            for(var i=0; i<cardNo.length; i++){
+            for(i=0; i<cardNo.length; i++){
                 if(cardNo.charCodeAt(i) >= 48 && cardNo.charCodeAt(i) <= 57)countNums++;
-                if(cardNo.charCodeAt(i)!==32 && cardNo.charCodeAt(i) < 48 || cardNo.charCodeAt(i) > 57)setCardNoErr(true);
+                if(cardNo.charCodeAt(i)!==32 && (cardNo.charCodeAt(i) < 48 || cardNo.charCodeAt(i) > 57))setCardNoErr(true);
             }
-            if(countNums != 16)setCardNoErr(true);
-            if(!cardNoErr && cardNo.charCodeAt(4)!==32 || cardNo.charCodeAt(9)!==32 || cardNo.charCodeAt(14)!==32){
+            if(countNums !== 16)setCardNoErr(true);
+            if(!cardNoErr && (cardNo.charCodeAt(4)!==32 || cardNo.charCodeAt(9)!==32 || cardNo.charCodeAt(14)!==32)){
                 setCardNoErr(true);
             }
 
@@ -58,14 +58,14 @@ export default function Form(props){
         //Expire Month Format Error
         if(expireMonth){
             if(expireMonth.length !== 2)setExpMonthErr(true);
-            for(var i=0; i<expireMonth.length; i++)
+            for(i=0; i<expireMonth.length; i++)
             if(expireMonth.charCodeAt(i) < 48 || expireMonth.charCodeAt(i) > 57)setExpMonthErr(true);
         }
 
         //Expire Year Format Error
         if(expireYear){
             if(expireYear.length !== 2)setExpYearErr(true);
-            for(var i=0; i<expireYear.length; i++){
+            for(i=0; i<expireYear.length; i++){
                 if(expireYear.charCodeAt(i) < 48 || expireYear.charCodeAt(i) > 57)setExpYearErr(true);
             }
         }
@@ -73,29 +73,27 @@ export default function Form(props){
         //Cvv Format Error
         if(cvv){
             if(cvv.length !== 3)setCvvErr(true);
-            for(var i=0; i<cvv.length; i++){
+            for(i=0; i<cvv.length; i++){
                 if(cvv.charCodeAt(i) < 48 || cvv.charCodeAt(i) > 57)setCvvErr(true);
             }
         }
-        if(!err && !fullNameErr && !cardNoErr && !expMonthErr && !expYearErr &&!cvvErr){
             props.setName(fullName);
-            props.setNum(cardNo);
-            props.setMonth(expireMonth);
-            props.setYear(expireYear);
-            props.setCVV(cvv);
+                props.setNum(cardNo);
+                props.setMonth(expireMonth);
+                props.setYear(expireYear);
+                props.setCVV(cvv);
 
+        toast.success("Success!", {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
             
-            toast.success("Success!", {
-                position: "top-center",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-                });
-        }
     }
 
 
@@ -140,7 +138,7 @@ export default function Form(props){
     </form>
     <ToastContainer
     position="top-center"
-    autoClose={5000}
+    autoClose={4000}
     hideProgressBar={false}
     newestOnTop={false}
     closeOnClick
